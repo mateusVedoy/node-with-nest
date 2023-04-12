7e05f653721d438594482d9017e424f5e5173bf8
mateusVedoy <mateusvedoy@outlook.com>
2023-04-11 23:28:57 -0300

    db

---
+++ src/infraestructure/local-repository/Tasks.ts
@@ -0,0 +1,10 @@
+export default {
+    "tasks": [
+        {
+            "user": "mateus",
+            "date": "2023-04-08",
+            "hour": "12:00",
+            "place": "Planet ball"
+        }
+    ]
+}
